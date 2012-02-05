4c92ff22ef909c8e0aeb31e125a78a5d5f141cd2
Chris Malton <chrism@cmalton.me.uk>
2012-02-05 08:17:14 +0000

    Add a basic no_mac system page (Resolves #1063)

---
+++ files/usr/share/sr-captive-portal/www/auth/no_mac.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>Student Robotics - No MAC address found</title>
+	</head>
+	<body>
+		<h1>No MAC address</h1>
+		<p>Unfortunately, we were unable to detect a MAC address for 
+your computer, which can uniquely identify your computer on this network.</p>
+		<p>Please contact the helpdesk, and give them the following
+information:</p>
+		<pre>
+No MAC Address
+IP: <?php echo $_SERVER["REMOTE_ADDR"]; ?>
+
+Date: <?php echo date("Y-m-d H:i:s"); ?>
+		</pre>
+	</body>
+</html>
